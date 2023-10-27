b9a98fafdd05891f24f85161855cc46d4e22ba52
Roman Veselý <vesely@xproduction.cz>
2023-10-27 09:50:13 +0200

    comment get product detail by code

--- frontend/models/ShoptetSync.php
+++ frontend/models/ShoptetSync.php
@@ -24,18 +24,11 @@ class ShoptetSync
             $guid = $item['guid'];
             $product = $this->api->getProductDetail($guid/*, ['include' => 'variants']*/);
 
-            foreach ($product['data']['variants'] as $variant) {
+            /*foreach ($product['data']['variants'] as $variant) {
                 $code = $variant['code'];
 
                 $productVariant = $this->api->getProductDetailByCode($code);
-
-                foreach ($productVariant as $key => $value) {
-                    //VarDumper::dump($key);
-                    //VarDumper::dump($value);
-                }
-
-                //exit;
-            }
+            }*/
 
             $model = Product::findOne(['guid' => $guid]) ?? new Product();
 
